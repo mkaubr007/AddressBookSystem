0857275a2767b640a38e4ecb83b59f126d621f6c
mkaubr007 <mkaubr007@gmail.com>
2021-09-18 01:00:23 +0530

    ContactsAndAddressBook added

--- src/main/java/com/biz/address/book/system/AddressBookMain.java
+++ src/main/java/com/biz/address/book/system/AddressBookMain.java
@@ -3,5 +3,6 @@ package com.biz.address.book.system;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System");
+        AddressBook addressBook=new AddressBook();
     }
 }
