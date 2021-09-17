a5d8b3813fc9db5ad056e8f0af499871abe15a12
mkaubr007 <mkaubr007@gmail.com>
2021-09-18 02:03:11 +0530

    AddContacts added

--- src/main/java/com/biz/address/book/system/AddressBook.java
+++ src/main/java/com/biz/address/book/system/AddressBook.java
@@ -4,6 +4,54 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    Scanner scan=new Scanner(System.in);
-    ArrayList<Contacts> list=new ArrayList<>();
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Contacts> list = new ArrayList<>();
+    public ArrayList<Contacts> arrayRead;
+
+    public void operation() {
+        list = arrayRead;
+        boolean status = true;
+        do {
+            System.out.println("Choose Operation you want to do");
+            System.out.println("1. Add");
+            switch (scan.nextInt()) {
+                case 1:
+                    add();
+                    break;
+            }
+        } while (status);
+    }
+
+    public void add() {
+        Contacts contacts = new Contacts();
+        System.out.println("Enter the First name:");
+        String firstName = scan.next();
+        contacts.setFirstName(firstName);
+
+        System.out.println("Enter the Last name:");
+        String lastName = scan.next();
+        contacts.setFirstName(lastName);
+
+        System.out.println("Enter the address:");
+        String address = scan.next();
+        contacts.setFirstName(address);
+
+        System.out.println("Enter the City:");
+        String city = scan.next();
+        contacts.setCity(city);
+
+        System.out.println("Enter the State:");
+        String state = scan.next();
+        contacts.setAddress(state);
+
+        System.out.println("Enter the Zip:");
+        Long zip = scan.nextLong();
+        contacts.setZip(zip);
+
+        System.out.println("Enter the Phone Number:");
+        Long phoneNumber = scan.nextLong();
+        contacts.setPhoneNumber(phoneNumber);
+        list.add(contacts);
+
+    }
 }
--- src/main/java/com/biz/address/book/system/AddressBookMain.java
+++ src/main/java/com/biz/address/book/system/AddressBookMain.java
@@ -1,8 +1,12 @@
 package com.biz.address.book.system;
 
+
 public class AddressBookMain {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System");
-        AddressBook addressBook=new AddressBook();
+        AddressBook addressBook = new AddressBook();
+        addressBook.operation();
     }
+
 }
