81caac33641ff6c722950e29b26daee2c68c0cbf
mkaubr007 <mkaubr007@gmail.com>
2021-09-18 02:45:41 +0530

    EditContacts added

--- src/main/java/com/biz/address/book/system/AddressBook.java
+++ src/main/java/com/biz/address/book/system/AddressBook.java
@@ -19,6 +19,8 @@ public class AddressBook {
                 case 1:
                     add();
                     break;
+                case 2:
+                    edit();
             }
         } while (status);
     }
@@ -101,6 +103,5 @@ public class AddressBook {
 
             }
         }
-
     }
 }
