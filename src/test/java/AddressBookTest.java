6d2048b28cb049cba5440ce9545d243265e0fafd
mkaubr007 <mkaubr007@gmail.com>
2021-09-26 14:43:50 +0530

    ContactsInCsvFile added

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class AddressBookTest {
     private static final String PATH_TXT="./src/main/resources/temp.txt";
+    private static final String PATH_CSV="./src/main/resources/temp.csv";
     @Test
     public void givenContactsEntries_WhenWrittenToFileShouldMatchAddressEntries() {
         List<Contacts> contactsList = new ArrayList<>();
@@ -20,6 +21,20 @@ public class AddressBookTest {
     public void givenContactsEntries_WhenReadingTotalFileShouldMatch(){
         AddressBook service=new AddressBook();
         long entries=service.readFile(PATH_TXT);
+        Assert.assertEquals(2,entries);
+    }
+    @Test
+    public void givenContactsEntries_WhenWrittenToCsvFileShouldMatchAddressEntries() {
+        List<Contacts> contactsList = new ArrayList<>();
+        contactsList.add(new Contacts("Manish", "Kumar", "Aurangabad", "Aurangabad", "Bihar", 42552l, 8002696700l, "mkaubr007@gmail.com"));
+        contactsList.add(new Contacts("Rohit", "Kumar", "Patna", "Patna", "Bihar", 3434l, 7646778838l, "rohitk01@gmail.com"));
+        AddressBook addressBook=new AddressBook(contactsList);
+        addressBook.writeFileData(PATH_CSV);
+    }
+    @Test
+    public void givenContactsEntries_WhenReadingTotalCsvFileShouldMatch(){
+        AddressBook service=new AddressBook();
+        long entries=service.readFile(PATH_CSV);
         Assert.assertEquals(2,entries);
     }
 
