45299d97213ddd83f9e156adb00c133a8b8a8b93
mkaubr007 <mkaubr007@gmail.com>
2021-09-27 00:15:06 +0530

    ContactsInJsonFile added

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -1,14 +1,20 @@
 import com.biz.address.book.system.AddressBook;
 import com.biz.address.book.system.Contacts;
+import com.opencsv.bean.CsvToBean;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBookTest {
     private static final String PATH_TXT="./src/main/resources/temp.txt";
     private static final String PATH_CSV="./src/main/resources/temp.csv";
+    private static final String PATH_JSON="./src/main/resources/temp.json";
     @Test
     public void givenContactsEntries_WhenWrittenToFileShouldMatchAddressEntries() {
         List<Contacts> contactsList = new ArrayList<>();
@@ -35,6 +41,20 @@ public class AddressBookTest {
     public void givenContactsEntries_WhenReadingTotalCsvFileShouldMatch(){
         AddressBook service=new AddressBook();
         long entries=service.readFile(PATH_CSV);
+        Assert.assertEquals(2,entries);
+    }
+    @Test
+    public void givenContactsEntries_WhenWrittenToJsonFileShouldMatchAddressEntries() {
+        List<Contacts> contactsList = new ArrayList<>();
+        contactsList.add(new Contacts("Manish", "Kumar", "Aurangabad", "Aurangabad", "Bihar", 42552l, 8002696700l, "mkaubr007@gmail.com"));
+        contactsList.add(new Contacts("Rohit", "Kumar", "Patna", "Patna", "Bihar", 3434l, 7646778838l, "rohitk01@gmail.com"));
+        AddressBook addressBook=new AddressBook(contactsList);
+        addressBook.writeFileData(PATH_JSON);
+    }
+    @Test
+    public void givenContactsEntries_WhenReadingTotalJsonFileShouldMatch(){
+        AddressBook service=new AddressBook();
+        long entries=service.readFile(PATH_JSON);
         Assert.assertEquals(2,entries);
     }
 
